67bfe171c1105be5645952aafb4e8856d1a7dc8c
NITHISHSENTHIL2025 <nithissenthil2025@gmail.com>
2026-03-01 03:11:18 +0530

    Fix authenticateToken crash

--- backend/server.js
+++ backend/server.js
@@ -52,6 +52,25 @@ const authMiddleware = (req, res, next) => {
     res.status(401).json({ message: "Token invalid." });
   }
 };
+const jwt = require("jsonwebtoken");
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Access token required" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid token" });
+    }
+
+    req.user = user;
+    next();
+  });
+}
 
 const adminOnly = (req, res, next) => {
   const authHeader = req.headers.authorization;
